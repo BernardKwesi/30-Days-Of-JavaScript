c456769ad61d5dba81c5fb3cffccd210350bf331
BernardKwesi <bernarddormon31@gmail.com>
2022-02-10 16:53:40 +0000

    .

--- 18_Day_Promises/18_day_starter/scripts/main.js
+++ 18_Day_Promises/18_day_starter/scripts/main.js
@@ -1,2 +1,50 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//console.log(countries)
+//alert('Open the console and check if the countries has been loaded')
+const countriesAPI = 'https://restcountries.com/v2/all'
+
+const fetchData = async () => {
+    try {
+      const response = await fetch(countriesAPI)
+      const countries = await response.json()
+      for (i in countries){
+        let languages ='' ;
+        for(j in countries[i].languages){
+            languages += countries[i].languages[j].name + ' ,';
+        }
+
+         let country = {
+             name : countries[i].name ,
+             capital:countries[i].capital,
+             languages : languages,
+             population : countries[i].population,
+             area : countries[i].area
+         } 
+          console.log(country)
+      }
+      //console.log(JSON.stringify(countries))
+     
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  console.log('===== async and await')
+  //fetchData()
+
+
+  // catsAPI
+let catName =[];
+  const catsAPI = 'https://api.thecatapi.com/v1/breeds';
+  const fetchCatData = async ()=>{
+    try{
+    const response = await fetch(catsAPI);
+    const data = await response.json();
+  
+    for(i in data){
+      catName.push(data[i].name)
+    }
+    }catch(err){
+      console.error(err);
+    }
+  }
+  //fetchCatData();
+  console.log(catName);
